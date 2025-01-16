bca9d36856a4780276c56b957959983e58a89fcb
manya-kaistha <130305667+manya-kaistha@users.noreply.github.com>
2025-01-16 15:36:55 +0530

    a part of day 16

---
+++ 16_Day/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+print(timmy)
+timmy.color("DarkGreen")
+timmy.shape("turtle")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
+
