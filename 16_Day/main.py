ade33bd97819ffacbd89b0332081365ea6faa5be
Manya Kaistha <manyakaistha@Manyas-Mac-mini.local>
2025-01-19 19:41:51 +0530

    Python Day 16 Complete

--- 16_Day/main.py
+++ 16_Day/main.py
@@ -1,14 +1,29 @@
-from turtle import Turtle, Screen
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-timmy = Turtle()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
-print(timmy)
-timmy.color("DarkGreen")
-timmy.shape("turtle")
-timmy.forward(100)
+is_on = True
 
-my_screen = Screen()
-print(my_screen.canvheight)
+money_machine.report()
+coffee_maker.report()
 
-my_screen.exitonclick()
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
+
 
